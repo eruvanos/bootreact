b2f84b501de03c50a92d0b189e52321e295e2e85
Maic Siemering <maic@vw-dilab.com>
2016-07-13 08:44:50 +0200

    remove generated files

--- default/app.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { render } from 'react-dom';
-import HelloWorld from './components/hello-world.jsx';
-
-render(
-    <HelloWorld />,
-    document.body
-);
--- default/components/hello-world.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
- 
-class HelloWorld extends React.Component {
-  render() {
-    return <p>Hello, world!1111</p>;
-  }
-}
- 
-export default HelloWorld;
